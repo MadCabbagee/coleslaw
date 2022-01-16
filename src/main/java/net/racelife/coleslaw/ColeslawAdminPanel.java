fed4dd8a45d6ce69f4819c3655b9e81558cb67db
Justin <justin.t.mclean@gmail.com>
2022-01-16 15:45:24 -0700

    rename class

--- src/main/java/net/racelife/coleslaw/ColeslawAdminPanel.java
+++ src/main/java/net/racelife/coleslaw/ColeslawAdminPanel.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ColeslawApplication {
+public class ColeslawAdminPanel {
 
     public static void main(String[] args) {
-        SpringApplication.run(ColeslawApplication.class, args);
+        SpringApplication.run(ColeslawAdminPanel.class, args);
     }
 
 }
