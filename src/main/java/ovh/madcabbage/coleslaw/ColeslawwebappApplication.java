aac7e4f7533a6f0dcf1aadeca41afb3223feee90
Justin <justin.t.mclean@gmail.com>
2022-01-16 00:49:39 -0700

    init project

---
+++ src/main/java/ovh/madcabbage/coleslaw/ColeslawwebappApplication.java
@@ -0,0 +1,13 @@
+package ovh.madcabbage.coleslaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColeslawwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColeslawwebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ovh/madcabbage/coleslaw/ColeslawwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ovh.madcabbage.coleslaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColeslawwebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
